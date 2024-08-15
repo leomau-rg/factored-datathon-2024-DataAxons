ede51a5079ee61653cd8fcbd7ad208c7dcefb861
Santiago Gonzalez <santiago.gm1191@gmail.com>
2024-08-14 18:19:57 -0600

    :sparkles: se puede descargar un conjunto de datos en un parquet por tema

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ subparsers = parser.add_subparsers(dest="command")
 extract_links_parser = subparsers.add_parser('extract_links')
 extract_links_parser.add_argument('file', type=str, help='path al archivo html con los datos')
 
-collect_stats_parser = subparsers.add_parser('collect_stats')
+collect_stats_parser = subparsers.add_parser('collect_by_theme', help='recolecta un conjunto de archivos csv en un solo parquet')
 collect_stats_parser.add_argument('file', type=str, help='path al archivo csv con los datos')
+collect_stats_parser.add_argument('output_dir', type=str, help='directorio donde se van a guardar los datos')
+collect_stats_parser.add_argument('theme', type=str, help='tema para filtrar')
+collect_stats_parser.add_argument('-l', "--limit", type=int, default=5, help='número de archivos a descargar')
 
 args = parser.parse_args()
 
@@ -19,7 +22,8 @@ if args.command == 'extract_links':
     
     scrtips.data_01_extract_links.process_raw_data_links(path)
 
-elif args.command == 'collect_stats':
+elif args.command == 'collect_by_theme':
     path = Path(args.file)
-    
-    scrtips.data_02_collect_stats.run(path)
+    output_dir = Path(args.output_dir)
+
+    scrtips.data_02_collect_by_theme.run(path, output_dir, args.theme, args.limit)
